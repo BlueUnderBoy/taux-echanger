60d3462d7c2c5cb49e2f4e78097abf98c92d5430
Jalen Young <120889301+BlueUnderBoy@users.noreply.github.com>
2024-09-18 02:03:11 +0000

    premier

--- app.rb
+++ app.rb
@@ -1,9 +1,22 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @data = HTTP.get("https://api.exchangerate.host/list?access_key=98b3818c779196980fc28a692dbb5f64")
+
+  @dstring = @data.to_s
+
+  @dparsed = JSON.parse(@dstring)
+
+  @dkeys = @dparsed.fetch("currencies")
+  erb(:homepage)
+end
+
+get("/:country") do
+
+  @code = params.fetch("country")
+
+  erb(:convert)
+
 end
